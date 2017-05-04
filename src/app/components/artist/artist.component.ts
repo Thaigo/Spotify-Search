7c66bfc3d81b5cef30d6674eb8e4152cb0bd6629
Thaigo Freitas <Thaigo Freitas>
2017-05-04 04:54:57 -0300

    add album artist details

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -22,13 +22,17 @@ export class ArtistComponent implements OnInit {
 
   ngOnInit() {
     this._route.params
-      .map(params => params['id'])
-      .subscribe((id) => {
-        this._spotifyService.getArtist(id)
-          .subscribe(artist => {
-            this.artist = artist;
-          })
-      });
+            .map(params => params['id'])
+            .subscribe((id) => {
+                this._spotifyService.getArtist(id)
+                    .subscribe(artist => {
+                        this.artist = artist;
+                    })
+                this._spotifyService.getAlbums(id)
+                    .subscribe(albums => {
+                        this.albums = albums.items;
+                    })
+            });
   }
 
 }
